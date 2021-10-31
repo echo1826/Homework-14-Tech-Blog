f4650e287d897437240b9d559bc2da06408b5c31
Ethan C <ethan.cho1826@gmail.com>
2021-10-31 17:18:29 -0500

    changed signup api route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,7 +3,7 @@ const {
     User
 } = require('../../models');
 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
         const newUser = User.create(req.body);
         req.session.save(() => {
