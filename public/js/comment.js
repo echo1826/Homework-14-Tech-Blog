50c0b9130f06f1b9389beac511cef2a27890f4d7
Ethan C <ethan.cho1826@gmail.com>
2021-11-02 00:00:15 -0500

    delete comment function defined

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,10 +1,27 @@
-async function addComment() {
+async function addComment(event) {
+    event.preventDefault()
     const content = document.querySelector('#newComment').value.trim();
     const postId = document.querySelector('#newComment').getAttribute('data-id');
     if(content) {
         const response = await fetch('/api/comment', {
             method: 'POST',
             body: JSON.stringify({content: content, post_id: postId}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if(response.ok) {
+            document.location.reload();
+        }else{
+            alert(response.statusText);
+        }
+    }
+}
+
+async function deleteComment(event) {
+    event.preventDefault();
+    const commentId = event.target.parentElement.getAttribute('data-id');
+    if(commentId) {
+        const response = await fetch(`api/comment/${commentId}`, {
+            method: 'DELETE',
             headers: {'Content-Type': 'application/json'}
         });
         if(response.ok) {
